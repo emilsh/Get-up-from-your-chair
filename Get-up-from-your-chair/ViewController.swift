aa22a8e4738ff9ac24398c65519c2bf122459edc
Emil Shafigin <shafigin@gmail.com>
2021-06-23 22:12:08 +0600

    Initial Commit

---
+++ Get-up-from-your-chair/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get-up-from-your-chair
+//
+//  Created by Emil Shafigin on 23/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Get-up-from-your-chairTests/Get_up_from_your_chairTests.swift
@@ -0,0 +1,33 @@
+//
+//  Get_up_from_your_chairTests.swift
+//  Get-up-from-your-chairTests
+//
+//  Created by Emil Shafigin on 23/6/21.
+//
+
+import XCTest
+@testable import Get_up_from_your_chair
+
+class Get_up_from_your_chairTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

05a8431b28d8f296d3e4ddc57b35f3fc32f70f4b
Emil Shafigin <shafigin@gmail.com>
2021-06-26 00:21:43 +0600

    Added Realm service methods

---
+++ Get-up-from-your-chair/Controllers/Main Screen/ActivityViewController+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityViewController+Extension.swift
+//  Get-up-from-your-chair
+//
+//  Created by Emil Shafigin on 26/6/21.
+//
+
+import UIKit
+
+extension ActivityViewController {
+  
+}
+
---
+++ Get-up-from-your-chair/Services/RealmService.swift
@@ -0,0 +1,52 @@
+//
+//  DataModel.swift
+//  Get-up-from-your-chair
+//
+//  Created by Emil Shafigin on 25/6/21.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmService {
+  private let oneDayInSeconds: TimeInterval = 86400
+  
+  static let shared = RealmService()
+  
+  private let realm: Realm
+  
+  private init() {
+    realm = try! Realm()
+  }
+  
+  func fetchDailyTasks(for date: TimeInterval) -> [Task] {
+    var dailyTasks: [Task] = []
+    
+    let currentDate = Date(timeIntervalSince1970: date)
+    guard let currentDay = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: currentDate) else {
+      fatalError("Cannot convert date")
+    }
+    let dateInterval = DateInterval(start: currentDay, duration: oneDayInSeconds)
+    
+    let tasks = realm.objects(Task.self).filter({ task in
+      dateInterval.contains(currentDate)
+    })
+    for task in tasks {
+      dailyTasks.append(task)
+    }
+    return dailyTasks
+  }
+  
+  func save(_ task: Task) {
+    task.id = lastId() + 1
+    try! realm.write {
+      realm.add(task)
+    }
+  }
+  
+  func lastId() -> Int {
+    guard let lastId = realm.objects(Task.self).last?.id else { return -1 }
+    return lastId
+  }
+
+}

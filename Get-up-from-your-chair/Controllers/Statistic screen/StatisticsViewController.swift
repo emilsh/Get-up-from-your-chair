5b44c5a106539966cde50e1f5e4a9faaa7568dc6
Sergey L <sergey43gm@gmail.com>
2021-06-27 12:18:01 +0300

    added placeholder label for statistics screen

--- Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController.swift
+++ Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 class StatisticsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  
+  private lazy var placeHolder: UILabel = {
+    let label = UILabel()
+    label.text = "Тут будет статистика"
+    label.font = .systemFont(ofSize: 40, weight: .semibold)
+    label.numberOfLines = 2
+    label.textAlignment = .center
+    return label
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .white
+    setupConstraints()
+  }
+  
+  private func setupConstraints() {
+    placeHolder.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(placeHolder)
+    
+    NSLayoutConstraint.activate([
+      placeHolder.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      placeHolder.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      placeHolder.widthAnchor.constraint(equalToConstant: 300)
+    ])
+  }
 }

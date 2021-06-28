c713779cd7c60032a621c00be2fd0417c11ab578
Emil Shafigin <shafigin@gmail.com>
2021-06-28 21:45:15 +0600

    Added statistics screen

---
+++ Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  StatisticsViewController+Extension.swift
+//  Get-up-from-your-chair
+//
+//  Created by Emil Shafigin on 27/6/21.
+//
+
+import UIKit
+
+extension StatisticsViewController {
+  
+}
--- Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController.swift
+++ Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController.swift
@@ -9,30 +9,44 @@ import UIKit
 
 class StatisticsViewController: UIViewController {
   
-  private lazy var placeHolder: UILabel = {
-    let label = UILabel()
-    label.text = "Тут будет статистика"
-    label.font = .systemFont(ofSize: 40, weight: .semibold)
-    label.numberOfLines = 2
-    label.textAlignment = .center
-    return label
-  }()
+  @IBOutlet weak var graphView: GraphView!
+  @IBOutlet weak var averageActivities: UILabel!
+  @IBOutlet weak var maxLabel: UILabel!
+  @IBOutlet weak var stackView: UIStackView!
+  
+//  private lazy var placeHolder: UILabel = {
+//    let label = UILabel()
+//    label.text = "Тут будет статистика"
+//    label.font = .systemFont(ofSize: 40, weight: .semibold)
+//    label.numberOfLines = 2
+//    label.textAlignment = .center
+//    return label
+//  }()
+  
+//  private lazy var graphView: GraphView = {
+//    let width:CGFloat = 300.0
+//    let x = self.view.frame.width / 2 - width / 2
+//    let frame = CGRect(x: x, y: 150, width: width, height: 250)
+//    let view = GraphView(frame: frame)
+//    view.backgroundColor = .clear
+//    return view
+//  }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = .white
-    setupConstraints()
+//    setupConstraints()
   }
   
-  private func setupConstraints() {
-    placeHolder.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(placeHolder)
-    
-    NSLayoutConstraint.activate([
-      placeHolder.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      placeHolder.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-      placeHolder.widthAnchor.constraint(equalToConstant: 300)
-    ])
-  }
+//  private func setupConstraints() {
+////    graphView.translatesAutoresizingMaskIntoConstraints = false
+//    view.addSubview(graphView)
+//
+//    NSLayoutConstraint.activate([
+//      graphView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//      graphView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//      graphView.widthAnchor.constraint(equalToConstant: 300)
+//    ])
+//  }
 }

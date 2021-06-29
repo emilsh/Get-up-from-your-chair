184e1fb7d7a321977351c8fb77616d3ff77fbf62
Emil Shafigin <shafigin@gmail.com>
2021-06-29 16:46:22 +0600

    Graph view included to viewcontroller

--- Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController.swift
+++ Get-up-from-your-chair/Controllers/Statistic screen/StatisticsViewController.swift
@@ -17,7 +17,31 @@ class StatisticsViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.backgroundColor = .white
+//    view.backgroundColor = .white
+    setupGraphDisplay()
+  }
+  
+  func setupGraphDisplay() {
+    let maxDayIndex = stackView.arrangedSubviews.count - 1
+   
+    graphView.setNeedsDisplay()
+    maxLabel.text = "\(graphView.graphPoints.max() ?? 0)"
+    
+    let average = graphView.graphPoints.reduce(0,+) / graphView.graphPoints.count
+    averageActivities.text = "\(average)"
+    
+    let today = Date()
+    let calendar = Calendar.current
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "ru_RU")
+    formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+    
+    for i in 0...maxDayIndex {
+      if let date = calendar.date(byAdding: .day, value: -i, to: today),
+         let label = stackView.arrangedSubviews[maxDayIndex - i] as? UILabel {
+        label.text = formatter.string(from: date)
+      }
+    }
   }
   
 
